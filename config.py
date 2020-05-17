4724c0703052da34b6a3ab0506e18ce54d604b09
Shenia <shenia.tung@gmail.com>
2020-05-17 11:47:19 -0700

    added flagging to prevent accidental opens, fixed flagging logic

---
+++ config.py
@@ -0,0 +1,4 @@
+IMAGE_DICTIONARY = ["img/0.png", "img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png"]
+BOMB_IMAGE = "img/space_black.png"
+FLAGGED_IMAGE = "img/flagged.png"
+INITIAL_IMAGE = "img/unopened.png"
--- minesweeper.py
+++ minesweeper.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from config import IMAGE_DICTIONARY, FLAGGED_IMAGE, BOMB_IMAGE, INITIAL_IMAGE
 
 def main():
     pygame.init() 
@@ -14,16 +15,17 @@ def main():
                 pygame.quit()
                 quit()
             elif event.type == pygame.MOUSEBUTTONUP:
-                position = pygame.mouse.get_pos()
-                if position[0] >= f.position[0] and position[0] < f.position[0] + f.width and position[1] >= f.position[1] and position[1] < f.position[1] + f.height:
-                    x = position[0] - f.position[0]
-                    y = position[1] - f.position[1]
-                    position_x = int((x - x % f.space_side_length) / f.space_side_length)
-                    position_y = int((y - y % f.space_side_length) / f.space_side_length)
+                mouse_position = pygame.mouse.get_pos()
+                if mouse_position[0] >= f.position[0] and mouse_position[0] < f.position[0] + f.width and mouse_position[1] >= f.position[1] and mouse_position[1] < f.position[1] + f.height:
+                    x = mouse_position[0] - f.position[0]
+                    y = mouse_position[1] - f.position[1]
+                    space_position_x = x // f.space_side_length
+                    space_position_y = y // f.space_side_length
+                    space = f.array_of_spaces[space_position_x][space_position_y]
                     if event.button == 1:
-                        f.array_of_spaces[position_x][position_y].open()
+                        space.open()
                     elif event.button == 3:
-                        f.array_of_spaces[position_x][position_y].flag()
+                        space.flag()
 
         gameDisplay.fill((10, 10, 10))
 
@@ -41,21 +43,26 @@ class Field:
         self.num_bombs = num_bombs
         self.height = height
         self.width = width
+        self.space_side_length = int(self.height/num_row)
         self.num_row = num_row
-        self.num_col = int(self.width / int(self.height / self.num_row))
-        self.space_side_length = int(self.height/self.num_row)
+        self.num_col = int(self.width/self.space_side_length)
         self.array_of_spaces = [[Space(False, x, y, self) for y in range(self.num_row)] for x in range(self.num_col)]
+        self.plant_bombs()
+
+    def plant_bombs(self):    
         rand_list = []
+
         for i in range(self.num_col * self.num_row):
             rand_list.append(i)
         
         for n in range(self.num_bombs):
-            rand_num = random.randint(0, self.num_col * self.num_row - 1 - n)
-            rand_num = rand_list.pop(rand_num) 
-            x = rand_num % self.num_col
-            y = int((rand_num - x) / self.num_col)
+            random_index = random.randint(0, self.num_col * self.num_row - 1 - n)
+            random_number = rand_list.pop(random_index) 
+            x = random_number % self.num_col
+            y = random_number // self.num_col
             self.array_of_spaces[x][y].has_mine = True
             print(n, x, y)
+        return
 
 # status in ["safe", "flagged", "unknown"]
 class Space:
@@ -67,84 +74,52 @@ class Space:
         self.field = field
         self.has_mine = has_mine
         self.opened = False
+        self.flagged = False
         self.position_x = position_x
         self.position_y = position_y
         self.side_length = self.field.space_side_length
         self.display_position_x = self.field.position[0] + self.position_x * self.side_length
         self.display_position_y = self.field.position[1] + self.position_y * self.side_length
-        self.image = pygame.image.load("img/unopened.png")
+        self.adjacent_space_positions = self.get_adjacent()
+        self.set_image(INITIAL_IMAGE)
+
+    def set_image(self, image_path):
+        self.image = pygame.image.load(image_path)
         self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
 
     def open(self):
-        if self.opened:
+        if self.opened or self.flagged:
             return
         elif self.has_mine:
             self.opened = True
-            self.image = pygame.image.load("img/space_black.png")
-            self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
+            self.flagged = False
+            self.set_image(BOMB_IMAGE)
             return
-        elif self.has_mine != True:
+        else:
+            self.opened = True
+            self.flagged = False
             num_mines = self.get_number()
             if num_mines == 0:
-                self.opened = True
-                self.image = pygame.image.load("img/0.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                check = self.get_adjacent()
-                for adjacent_space_position in check:
+                self.set_image(IMAGE_DICTIONARY[0])
+                for adjacent_space_position in self.adjacent_space_positions:
                     if self.field.array_of_spaces[adjacent_space_position[0]][adjacent_space_position[1]].opened == False and self.field.array_of_spaces[adjacent_space_position[0]][adjacent_space_position[1]].has_mine == False:
                         self.field.array_of_spaces[adjacent_space_position[0]][adjacent_space_position[1]].open()
-                    else:
-                        pass
                 return 
-            elif num_mines == 1:
-                self.opened = True
-                self.image = pygame.image.load("img/1.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                return 
-            elif num_mines == 2:
-                self.opened = True
-                self.image = pygame.image.load("img/2.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                return 
-            elif num_mines == 3:
-                self.opened = True
-                self.image = pygame.image.load("img/3.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                return 
-            elif num_mines == 4:
-                self.opened = True
-                self.image = pygame.image.load("img/4.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                return 
-            elif num_mines == 5:
-                self.opened = True
-                self.image = pygame.image.load("img/5.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                return 
-            elif num_mines == 6:
-                self.opened = True
-                self.image = pygame.image.load("img/6.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                return 
-            elif num_mines == 7:
-                self.opened = True
-                self.image = pygame.image.load("img/7.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                return 
-            elif num_mines == 8:
-                self.opened = True
-                self.image = pygame.image.load("img/8.png")
-                self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
-                return 
+            elif num_mines >= 1 and num_mines <= 8:
+                self.set_image(IMAGE_DICTIONARY[num_mines])
+                return
             else:
-                raise ValueError("Too many mines!")     
+                raise ValueError("Invalid number of mines")     
     
     def flag(self):
         if self.opened:
             return
+        elif self.flagged:
+            self.flagged = False
+            self.set_image(INITIAL_IMAGE)
         else:
-            self.image = pygame.image.load("img/flagged.png")
-            self.image = pygame.transform.scale(self.image, (self.side_length, self.side_length))
+            self.flagged = True
+            self.set_image(FLAGGED_IMAGE)
             return
     
     def get_adjacent(self):
@@ -207,7 +182,7 @@ class Space:
             return -1
         else:
             num = 0
-            check = self.get_adjacent()
+            check = self.adjacent_space_positions
             for position in check:
                 if self.field.array_of_spaces[position[0]][position[1]].has_mine:
                     num = num + 1
